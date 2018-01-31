44b2f6c9a4d8d7953f816b4c2276fbf4b817687c
egiorio <giorio106@gmail.com>
2018-01-31 11:54:30 +0100

    Add files via upload

--- home.php
+++ home.php
@@ -22,11 +22,11 @@
     <div id="corpo">
       <h2>La nostra scuola</h2>
       <p>The English World, fondata nel 2004, è una struttura internazionale privata che offre un ambiente formativo a misura di studente. La Scuola prevede un approccio dinamico, differenziato, stimolante e gratificante. Gli studenti sono supportati per esprimere al massimo il loro potenziale con un'attenzione specifica al mantenimento e rinforzo positivo della loro autostima. Lo scopo della nostra scuola e' dare ai nostri studenti una padronanza della lingua inglese che li renda autosufficienti nel dialogo e nella comprensione della lingua.</p>
-
+	  <img src="img/img1.jpg" class="valid" alt="Immagine di abbellimento"/>
       <h2>I nostri corsi</h2>
       <p>Ogni studente puo' scegliere il corso che fa per lui, partendo da corsi base, corsi intermedi e corsi elevati. Ogni corso e' tenuto da un docente madrelingua che punta ad insegnarvi i trucchi e la bellezza di questa lingua mondiale.</br>
 Siamo sicuri che nella nostra scuola troverete il corso che fa al vostro caso e sarete soddisfatti del nostro lavoro!</p>
-
+	<img src="img/img2.jpg" class="valid" alt="Immagine di abbellimento"/>
     </div>
     <div id="footer">
         <img src="img/css.bmp" class="valid" alt="CSS Valid!"/>
